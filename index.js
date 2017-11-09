8d627384389214f1ab368a04338e9b55bdfe48f6
Obi <obinnawalker@gmail.com>
2017-11-09 21:13:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 function findMatching(drivers, name){
   return drivers.filter(function(e){
     //name.toLowerCase()
-    if(e.toLowerCase == name.toLowerCase)
-    return e == name
+
+    return e == (/name/i)
   })
 }
 function fuzzyMatch(drivers, subName){
