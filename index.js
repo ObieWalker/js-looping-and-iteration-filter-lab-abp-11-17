23ff1b94ef529f78d6a956981a07c6665fdd763b
Obi <obinnawalker@gmail.com>
2017-11-09 20:52:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 function findMatching(drivers, name){
   return drivers.filter(function(e){
     //name.toLowerCase()
+    if(e.toLowerCase == name.toLowerCase)
     return e === name
   })
 }
