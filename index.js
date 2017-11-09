79ed86028a4790e99823bd95bd637b44ba335f69
Obi <obinnawalker@gmail.com>
2017-11-09 20:49:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file
+function findMatching(drivers, name){
+  return drivers.filter(function(e){
+    name.toLowerCase()
+    return e.name.toLowerCase === name
+  })
+}
